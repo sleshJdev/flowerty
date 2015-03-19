3f8c31401c6ff2da082835a231e8417f41c5d8ed
unknown <m.tatarintseva.mt@gmail.com>
2015-03-19 22:41:36 +0300

    first commit

---
+++ src/main/java/by/itechart/flowerty/domain/User.java
@@ -0,0 +1,11 @@
+package by.itechart.flowerty.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Мария
+ * Date: 19.03.15
+ * Time: 22:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+}
