a25bd6631161b94c2a0bdd35ca2af5505db2bfb7
unknown <m.tatarintseva.mt@gmail.com>
2015-03-21 22:55:38 +0300

    0321

---
+++ src/main/java/by/itechart/flowerty/dao/AbstractDao.java
@@ -0,0 +1,11 @@
+package by.itechart.flowerty.dao;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Мария
+ * Date: 21.03.15
+ * Time: 17:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class AbstractDao {
+}
---
+++ src/main/java/by/itechart/flowerty/domain/Order.java
@@ -0,0 +1,11 @@
+package by.itechart.flowerty.domain;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Мария
+ * Date: 20.03.15
+ * Time: 22:40
+ * To change this template use File | Settings | File Templates.
+ */
+public class Order {
+}
