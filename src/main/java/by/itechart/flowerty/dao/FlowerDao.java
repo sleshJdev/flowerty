a7e5538157abedbf914ee6c2665030437f127f06
unknown <m.tatarintseva.mt@gmail.com>
2015-03-23 00:05:06 +0300

    2303

--- src/main/java/by/itechart/flowerty/dao/FlowerDao.java
+++ src/main/java/by/itechart/flowerty/dao/FlowerDao.java
@@ -1,11 +1,11 @@
-package by.itechart.flowerty.service;
+package by.itechart.flowerty.dao;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Мария
- * Date: 21.03.15
- * Time: 20:21
+ * Date: 22.03.15
+ * Time: 21:42
  * To change this template use File | Settings | File Templates.
  */
-public class OrderService {
+public class FlowerDao {
 }
--- src/main/java/by/itechart/flowerty/service/FlowerService.java
+++ src/main/java/by/itechart/flowerty/service/FlowerService.java
@@ -3,9 +3,10 @@ package by.itechart.flowerty.service;
 /**
  * Created with IntelliJ IDEA.
  * User: Мария
- * Date: 21.03.15
- * Time: 20:21
+ * Date: 22.03.15
+ * Time: 18:46
  * To change this template use File | Settings | File Templates.
  */
-public class OrderService {
+public class FlowerService {
+
 }
