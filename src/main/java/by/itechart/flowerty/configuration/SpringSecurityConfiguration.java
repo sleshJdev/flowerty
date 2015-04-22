a5b52b9be8bb18ac338a8c6e284819fe9a1ecdfb
Rostislav-Bobrovsky <rostislav.bobrovsky@gmail.com>
2015-04-23 02:51:54 +0300

    configure spring security from database

--- src/main/java/by/itechart/flowerty/configuration/SpringSecurityConfiguration.java
+++ src/main/java/by/itechart/flowerty/configuration/SpringSecurityConfiguration.java
@@ -1,5 +1,8 @@
 package by.itechart.flowerty.configuration;
 
+import by.itechart.flowerty.dao.repository.RoleRepository;
+import by.itechart.flowerty.model.Right;
+import by.itechart.flowerty.model.Role;
 import by.itechart.flowerty.security.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -16,6 +19,10 @@ import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
 import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
 /**
  * Created by Rostislav on 26-Mar-15
  */
@@ -31,6 +38,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private EntryPointUnauthorizedHandler unauthorizedHandler;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authenticator());
@@ -43,21 +53,33 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-//                .addFilterBefore(new CustomUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .authorizeRequests()
-                .antMatchers("/user/list/**")
-                .access("hasRole('ROLE_ADMIN')")
+        List<Role> roles = roleRepository.findAll();
+        for (int i = 0; i < roles.size(); ++i) {
+            Set<Right> rights = roles.get(i).getRights();
+            for (Iterator<Right> it = rights.iterator(); it.hasNext(); ) {
+                Right right = it.next();
+                http
+                        .authorizeRequests()
+                        .antMatchers("/" + right.getName() + "/**")
+                        .access("hasRole('ROLE_" + roles.get(i).getName() + "')")
                 ;
+            }
+        }
+
+//        http
+////                .addFilterBefore(new CustomUsernamePasswordAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+//                .authorizeRequests()
+//                .antMatchers("/user/**")
+//                .access("hasRole('ROLE_ADMIN')")
+//                ;
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/contact/**")
+//                .access("hasRole('ROLE_SUPERVISOR')")
+//                ;
 
         http
-                .authorizeRequests()
-                .antMatchers("/contact/**")
-                .access("hasRole('ROLE_SUPERVISOR')")
-                ;
-
-        http
-//            .and()
                 .rememberMe()
                 .rememberMeServices(rememberMeServices())
                 .key(KEY)
