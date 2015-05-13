a908f37a767806ecc7706fd678430330dff78757
sleshJdev <slesh.eugene93@gmail.com>
2015-05-13 03:29:02 +0300

    search of contact refactoring: remove boiler place code. improve logic.

--- src/main/webapp/resources/js/app/contact/service/ContactSearchService.js
+++ src/main/webapp/resources/js/app/contact/service/ContactSearchService.js
@@ -2,30 +2,30 @@
 /**
  * Created by Катерина on 11.05.2015.
  */
+angular.module("flowertyApplication.contactModule")
 
-angular.module("flowertyApplication.contactModule").service("contactSearchService", ['$http',
-    function ($http) {
+.service("contactSearchService", ['$http', function ($http) {
 
-        var service = this;
+    var service = this;
 
-        var contactToSearch = {};
+    var contactToSearch = {};
 
-        service.setContactToSearch = function(contact) {
-            contactToSearch = contact;
-        };
+    service.setContactToSearch = function(contact) {
+        contactToSearch = contact;
+    };
 
-        service.searchContact = function (page, limit, successCallback, errorCallback) {
-            $http({
-                method: "post",
-                url: "contact/search/" + page + "/" + limit,
-                data: contactToSearch
-            })
-                .success(successCallback)
-                .error(function(data) {
-                    console.log("error search contact. details: " + JSON.stringify(data));
-                    errorCallback(data);
-                }
-            );
-        };
+    service.searchContact = function (page, limit, successCallback, errorCallback) {
+        $http({
+            method: "post",
+            url: "contact/search/" + page + "/" + limit,
+            data: contactToSearch
+        })
+            .success(successCallback)
+            .error(function(data) {
+                console.log("error search contact. details: " + JSON.stringify(data));
+                errorCallback(data);
+            }
+        );
+    };
 
-    }]);
+}]);
